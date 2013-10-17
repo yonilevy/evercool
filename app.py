525aa9ec02854304e61e48ffc567f21b4fcfafe8
yonilevy <yonilevy@gmail.com>
2013-10-17 18:13:37 +0300

    server: try to use env vars for state to make it faster

--- app.py
+++ app.py
@@ -7,18 +7,23 @@ app = Flask(__name__)
 is_local = os.getenv('MONGOHQ_URL') is None
 
 current_state = None
-TURN_ON = "TURN_ON"
-TURN_OFF = "TURN_OFF"
+TURN_ON = 'TURN_ON'
+TURN_OFF = 'TURN_OFF'
+EVERCOOL_COMMAND = 'EVERCOOL_COMMAND'
+
 
 @app.route('/')
 def hello():
+    env_command = os.getenv(EVERCOOL_COMMAND)
     command = mongo.state.find_one()
     return """Hello World!!!<br>
     Current state: %s<br>
     <a href="turn_on">turn on</a><br>
     <a href="turn_off">turn off</a><br>
-    <a href="pop_command">pop command</a>
-    """ % command
+    <a href="pop_command">pop command</a><br>
+    debug: %s
+    """ % (command, env_command)
+
 
 @app.route('/pop_command')
 def pop_command():
@@ -28,13 +33,17 @@ def pop_command():
     mongo.state.remove(state)
     return state.get('command')
 
+
 @app.route('/turn_on')
 def turn_on():
+    os.environ[EVERCOOL_COMMAND] = TURN_ON
     mongo.state.insert({"command": TURN_ON})
     return "turning on"
 
+
 @app.route('/turn_off')
 def turn_off():
+    os.environ[EVERCOOL_COMMAND] = TURN_OFF
     mongo.state.insert({"command": TURN_OFF})
     return "turning off"
 
