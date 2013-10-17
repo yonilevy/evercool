d8235460f58e735ecb50df54d8f7202935e37cc6
yonilevy <yonilevy@gmail.com>
2013-10-17 18:26:05 +0300

    server: use env vars rather than mongo to keep state

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask
-from mongo import mongo
 
 app = Flask(__name__)
 
@@ -9,43 +8,47 @@ is_local = os.getenv('MONGOHQ_URL') is None
 current_state = None
 TURN_ON = 'TURN_ON'
 TURN_OFF = 'TURN_OFF'
+NOP = ''
 EVERCOOL_COMMAND = 'EVERCOOL_COMMAND'
 
 
 @app.route('/')
 def hello():
-    env_command = os.getenv(EVERCOOL_COMMAND)
-    command = mongo.state.find_one()
+    command = get_current_command()
     return """Hello World!!!<br>
     Current state: %s<br>
     <a href="turn_on">turn on</a><br>
     <a href="turn_off">turn off</a><br>
     <a href="pop_command">pop command</a><br>
-    debug: %s
-    """ % (command, env_command)
+    """ % command
 
 
 @app.route('/pop_command')
 def pop_command():
-    state = mongo.state.find_one()
-    if not state:
-        return ''
-    mongo.state.remove(state)
-    return state.get('command')
+    command = get_current_command()
+    set_current_command(NOP)
+    return command
 
 
 @app.route('/turn_on')
 def turn_on():
-    os.environ[EVERCOOL_COMMAND] = TURN_ON
-    mongo.state.insert({"command": TURN_ON})
+    set_current_command(TURN_ON)
     return "turning on"
 
 
 @app.route('/turn_off')
 def turn_off():
-    os.environ[EVERCOOL_COMMAND] = TURN_OFF
-    mongo.state.insert({"command": TURN_OFF})
+    set_current_command(TURN_OFF)
     return "turning off"
+
+
+def get_current_command():
+    val = os.getenv(EVERCOOL_COMMAND)
+    return val if val else ''
+
+
+def set_current_command(command):
+    os.environ[EVERCOOL_COMMAND] = command
 
 
 if __name__ == '__main__':
